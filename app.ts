4e842827badb88d9376fa746e1011635f01329a4
Abel Misiocha <wells.mail.13@gmail.com>
2025-09-29 11:09:00 +0300

    feat: Remove JSON body size limit from express.json middleware

--- app.ts
+++ app.ts
@@ -33,7 +33,7 @@ app.use(
 
 app.use(helmet(helmetConfig));
 app.use(Borgen({}));
-app.use(express.json({ limit: "200mb" }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true, limit: "200mb" }));
 app.use(express.static(path.join(__dirname, "public")));
